43d2ae7cb1d1960839f03db29cc24e452db0e0cb
Robot-Inventor <50550561+Robot-Inventor@users.noreply.github.com>
2025-12-24 02:48:26 +0900

    chore: remove unused `ignores` in the ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,3 @@
 import { eslintConfig } from "@robot-inventor/eslint-config";
 
-export default [
-    {
-        ignores: [
-            "**/*.guard.ts",
-        ]
-    },
-    ...eslintConfig
-];
+export default eslintConfig;
